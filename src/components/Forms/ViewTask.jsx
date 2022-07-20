b0faf6d901d9a504f908f848d681a6f9e196a9ee
JJooaa <leitola.joa@gmail.com>
2022-07-20 14:52:20 +0300

    better viewtask + styling

--- src/components/Forms/ViewTask.jsx
+++ src/components/Forms/ViewTask.jsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../../styles/form.css";
+import checkIcon from "../../assets/icon-check.svg";
 
 const ViewTask = ({ selectedTask }) => {
+  const [amountOfCompletion, setAmountOfCompletion] = useState(0);
+  useEffect(() => {
+    setAmountOfCompletion(
+      selectedTask.subtasks.filter((item) => item.isCompleted === true).length
+    );
+  }, [selectedTask]);
+
   return (
     <div className="view-task-content">
       <h1>{selectedTask.title}</h1>
       <h3>{selectedTask.description}</h3>
+      <div>
+        <h4>
+          Subtasks ({amountOfCompletion} of {selectedTask.subtasks.length})
+        </h4>
+        {selectedTask.subtasks.map((task) => (
+          <div className={`subtask ${task.isCompleted && "line-through"}`}>
+            <img
+              src={task.isCompleted && checkIcon}
+              alt=""
+              className={task.isCompleted ? "completed" : "not-completed"}
+            />
+            {task.title}
+          </div>
+        ))}
+      </div>
+      <h4>Current Status</h4>
     </div>
   );
 };
